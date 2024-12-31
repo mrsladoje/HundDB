b1e705ad88a89573621f6ce5754b1f22a4a5912b
Vukan Radojevic <vukanradojevicc@gmail.com>
2024-12-31 04:11:31 +0100

    Refactor: Remove HashToIntArray function and optimize SimHash implementation

--- structures/sim_hash/sim_hash.go
+++ structures/sim_hash/sim_hash.go
@@ -6,23 +6,6 @@ import (
 	"strings"
 )
 
-// Helper to hash a word to a 64-bit integer array
-func HashToIntArray(word string) []int64 {
-	hasher := fnv.New64a()
-	hasher.Write([]byte(word))
-	hash := hasher.Sum64()
-
-	bits := make([]int64, 64)
-	for i := 0; i < 64; i++ {
-		if (hash & (1 << (63 - i))) != 0 {
-			bits[i] = 1
-		} else {
-			bits[i] = -1
-		}
-	}
-	return bits
-}
-
 func GenerateWordFrequency(text string) map[string]uint32 {
 	wordFrequency := make(map[string]uint32)
 	words := strings.Fields(strings.ToLower(text)) // TODO: Unapredi tokenizerom u utils-u
@@ -35,10 +18,19 @@ func GenerateWordFrequency(text string) map[string]uint32 {
 func SimHash(text string) uint64 {
 	wordFrequency := GenerateWordFrequency(text)
 	sum := make([]int64, 64)
+	hasher := fnv.New64a()
+
 	for word, count := range wordFrequency {
-		bitArray := HashToIntArray(word)
+		hasher.Reset()
+		hasher.Write([]byte(word))
+		hash := hasher.Sum64()
+
 		for i := 0; i < 64; i++ {
-			sum[i] += bitArray[i] * int64(count)
+			bit := int64(1)
+			if (hash & (1 << (63 - i))) == 0 {
+				bit = -1
+			}
+			sum[i] += bit * int64(count)
 		}
 	}
 
