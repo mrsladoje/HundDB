58f587240ebf7610680451397705783b58dc7e32
Vukan Radojevic <vukanradojevicc@gmail.com>
2024-12-31 03:12:45 +0100

    Refactor: Change package name to 'sim_hash' and improve SimHash and word frequency functions

--- structures/sim_hash/sim_hash.go
+++ structures/sim_hash/sim_hash.go
@@ -1,17 +1,18 @@
-package main
+package sim_hash
 
 import (
 	"hash/fnv"
+	"math/bits"
 	"strings"
 )
 
-// helper to hash a word to a 64-bit integer array
-func HashToIntArray(word string) []int {
+// Helper to hash a word to a 64-bit integer array
+func HashToIntArray(word string) []int64 {
 	hasher := fnv.New64a()
 	hasher.Write([]byte(word))
 	hash := hasher.Sum64()
 
-	bits := make([]int, 64)
+	bits := make([]int64, 64)
 	for i := 0; i < 64; i++ {
 		if (hash & (1 << (63 - i))) != 0 {
 			bits[i] = 1
@@ -22,42 +23,35 @@ func HashToIntArray(word string) []int {
 	return bits
 }
 
-func SimHash(text string) uint64 {
-	wordCounts := make(map[string]int)
-	words := strings.Fields(strings.ToLower(text)) // Split by spaces and lowercase
-
-	for _, word := range words { // count the frequency of each word
-		wordCounts[word]++
+func GenerateWordFrequency(text string) map[string]uint32 {
+	wordFrequency := make(map[string]uint32)
+	words := strings.Fields(strings.ToLower(text)) // TODO: Unapredi tokenizerom u utils-u
+	for _, word := range words {
+		wordFrequency[word]++
 	}
+	return wordFrequency
+}
 
-	sum := make([]int, 64) // 64-bit fingerprint
-
-	for word, count := range wordCounts {
+func SimHash(text string) uint64 {
+	wordFrequency := GenerateWordFrequency(text)
+	sum := make([]int64, 64)
+	for word, count := range wordFrequency {
 		bitArray := HashToIntArray(word)
 		for i := 0; i < 64; i++ {
-			sum[i] += bitArray[i] * count // Weight by word frequency
+			sum[i] += bitArray[i] * int64(count)
 		}
 	}
 
-	var fingerprint uint64
+	fingerprint := uint64(0)
 	for i, value := range sum {
-		if value > 0 {
-			fingerprint |= (1 << (63 - i)) // Set bit to 1 if positive
+		if value >= 0 {
+			fingerprint |= (1 << (63 - i))
 		}
 	}
-
 	return fingerprint
 }
 
-func HammingDistance(fp1, fp2 uint64) int {
-
-	diff := fp1 ^ fp2
-
-	count := 0
-	for diff != 0 {
-		count += int(diff & 1) // Add 1 if the least significant bit is set
-		diff >>= 1             // Shift the bits to the right
-	}
-
-	return count
+func HammingDistance(fingerprint1, fingerprint2 uint64) uint8 {
+	diff := fingerprint1 ^ fingerprint2
+	return uint8(bits.OnesCount64(diff))
 }
