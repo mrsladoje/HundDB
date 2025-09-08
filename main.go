529d9a7ab9ebc3c4036d34f9cb1ea491bfda50dd
Marko Sladojevic <mrsladoje@gmail.com>
2025-09-09 00:18:44 +0200

    Add: Wails with React + Vite + Tailwind

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:gui/dist
+var assets embed.FS
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts up
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Example method - you can add your own methods here
+func (a *App) Greet(name string) string {
+	return "Hello " + name + ", It's show time!"
+}
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "HundDB",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind:             []interface{}{app}, // Bind your app methods to frontend
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
